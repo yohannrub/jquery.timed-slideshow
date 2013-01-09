48bcd22011ca9cac5d069f1fd524c323ba5b9973
yohannrub <yohannrub@users.noreply.github.com>
2013-01-09 19:29:19 +0100

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,91 @@
-jquery.slideshow
-================
+# jQuery Slideshow
 
-A lightweight jQuery slideshow plugin
+jQuery Slideshow is a lightweight jQuery slideshow plugin with timing animation controls and automatic pagination features.  
+This plugin has available functions to go to a specific slide, play, pause and reset the internal timer.  
+Its timer uses an actual timekeeping by polling current time (as use of `setInterval()` only is not fully reliable for actual timekeeping).  
+It currently supports *slide* and *fade* transition effects.
+
+[**Demo page**](http://htmlpreview.github.com/?https://github.com/yohannrub/jquery.slideshow/blob/master/example/index.html)
+
+
+## Usage
+
+Apply a specific CSS class (defaults to 'slideshow-slides', see *Options*) to your list of slides, and wrap a parent block element around it.  
+You can also add *prev*/*next* buttons and pagination elements by applying specific CSS classes (see *Options*).  
+You can apply the slideshow functions to the parent block element.
+
+```html
+<div id="slideshow-container">
+    <ul class="slideshow-slides">
+        <li><img src="img/image1.jpg" /></li>
+        <li><img src="img/image2.jpg" /></li>
+        <li><img src="img/image3.jpg" /></li>
+        <li><img src="img/image4.jpg" /></li>
+    </ul>
+    <div>
+        <div class="slideshow-pagination"></div>
+        <a class="slideshow-prev">Prev</a>
+        <a class="slideshow-next">Next</a>
+    </div>
+</div>
+```
+
+```javascript
+// Initialize the slideshow for #slideshow-container
+$('#slideshow-container').slideshow();
+
+// Will play the slideshow
+$('#slideshow-container').slideshow('play');
+
+// Will pause the timer of the current slide
+$('#slideshow-container').slideshow('pause');
+
+// Will reset the timer of the current slide
+$('#slideshow-container').slideshow('reset');
+
+// Will go to first slide (parameter can be any integer between 0 and [number of slides]-1)
+$('#slideshow-container').slideshow('slide', 0);
+
+// Will retrieve the current slide index (integer between 0 and [number of slides]-1)
+var currentIndex = $('#slideshow-container').slideshow('getCurrentIndex');
+```
+
+
+## Options
+
+Some options can be passed at initialization (the following values are defaults):
+
+```javascript
+// Initialize the slideshow for #slideshow-container with passed options
+$('#slideshow-container').slideshow({
+    autoPlay: true,                          // whether or not to auto-play at initialization
+    displayDuration: 5000,                   // duration of display of each slide (in ms)
+    transitionDuration: 500,                 // duration of transition between each slide (in ms)
+    transitionEffect: 'slide',               // effect of transition ('slide' or 'fade')
+    transitionDirectionSlide: 'horizontal',  // direction of transition when using 'slide' effect ('horizontal' or 'vertical')
+    transitionCrossfade: false,              // whether or not to crossfade when using 'fade' effect
+    transitionStep: 1,                       // step of transition (positive or negative integer)
+    transitionEasing: null,                  // easing of transition
+    transitionStartCallback: function(){},   // function to be called at start of each transition
+    transitionEndCallback: function(){},     // function to be called at end of each transition
+    startIndex: 0,                           // index of the first slide to display
+    slidesClass: 'slideshow-slides',         // CSS class of slides list element
+    prevClass: 'slideshow-prev',             // CSS class of previous button element
+    nextClass: 'slideshow-next',             // CSS class of next button element
+    paginationClass: 'slideshow-pagination'  // CSS class of pagination container element
+});
+```
+
+
+## Styling
+
+The current active slide and pagination item elements can be styled using the 'active' CSS class.
+
+```css
+#slideshow-container .slideshow-slides .active {
+  background-color: #ff0000;
+}
+#slideshow-container .slideshow-pagination ul li.active {
+  background-color: #ff0000;
+}
+```
---
+++ jquery.slideshow.js
@@ -0,0 +1,235 @@
+/*!
+ * jQuery Slideshow v@1.0
+ * https://github.com/yohannrub/jquery.slideshow
+ * Licensed under the MIT license
+ */
+
+;(function($) {
+
+    var DEFAULT_SETTINGS = {
+        autoPlay: true,
+        displayDuration: 5000,
+        transitionDuration: 500,
+        transitionEffect: 'slide',
+        transitionDirectionSlide: 'horizontal',
+        transitionCrossfade: false,
+        transitionStep: 1,
+        transitionEasing: null,
+        transitionStartCallback: $.noop,
+        transitionEndCallback: $.noop,
+        startIndex: 0,
+        slidesClass: 'slideshow-slides',
+        prevClass: 'slideshow-prev',
+        nextClass: 'slideshow-next',
+        paginationClass: 'slideshow-pagination'
+    };
+
+    var GLOBALS = {};
+    GLOBALS.activeClass = 'active';
+
+    var namespace = 'slideshow';
+
+    var methods = {
+        init: function(options) {
+            options = $.extend({}, DEFAULT_SETTINGS, options);
+
+            return this.each(function() {
+                var $this = $(this);
+                var data = $.extend({}, options);
+
+                var transitionDirectionSlideVertical = (data.transitionDirectionSlide == 'vertical');
+
+                data.isPlaying = false;
+                data.timer = null;
+                data.timeStart = null;
+                data.timeRemaining = data.displayDuration;
+                data.transitionRunning = false;
+                data.transitionCssProperty = transitionDirectionSlideVertical ? 'top' : 'left';
+                data.sizeCssProperty = transitionDirectionSlideVertical ? 'height' : 'width';
+                data.$slidesUL = $this.find('.' + data.slidesClass);
+                data.$slidesLI = data.$slidesUL.children();
+                data.$paginationLI = null;
+                data.currentIndex = mod(data.startIndex, data.$slidesLI.length);
+
+                var $prevButton = $this.find('.' + data.prevClass),
+                    $nextButton = $this.find('.' + data.nextClass),
+                    $pagination = $this.find('.' + data.paginationClass);
+                var $slidesContainer = $('<div></div>');
+
+                $slidesContainer.css({
+                    'overflow': 'hidden'
+                });
+                data.$slidesUL.css({
+                    'margin': 0,
+                    'padding': 0,
+                    'position': 'relative',
+                    'list-style': 'none'
+                });
+
+                if (data.transitionEffect == 'fade') {
+                    data.$slidesLI.css({
+                        'position': 'absolute',
+                        'z-index': 0,
+                        'opacity': 0
+                    });
+                    data.$slidesLI.eq(data.currentIndex).css({
+                        'opacity': 1
+                    });
+                    data.$slidesUL.css({
+                        'height': Math.max.apply(null, data.$slidesLI.map(function () {return $(this).outerHeight(true);}).get())
+                    });
+                    if (!data.transitionCrossfade) {
+                        data.transitionDuration = data.transitionDuration / 2;
+                    }
+                } else {
+                    data.$slidesLI.css({
+                        'float': transitionDirectionSlideVertical ? 'none' : 'left'
+                    });
+                    data.slidesLISize = transitionDirectionSlideVertical ? data.$slidesLI.outerHeight(true) : data.$slidesLI.outerWidth(true);
+                    data.$slidesUL.css(data.sizeCssProperty, data.slidesLISize * data.$slidesLI.length)
+                        .css(data.transitionCssProperty, -data.currentIndex * data.slidesLISize);
+                    $slidesContainer.css(data.sizeCssProperty, data.slidesLISize);
+                }
+
+                data.$slidesLI.eq(data.currentIndex).addClass(GLOBALS.activeClass);
+                data.$slidesUL.wrap($slidesContainer);
+
+                $prevButton.on('click', function() {
+                    $this[namespace]('slide', data.currentIndex - data.transitionStep);
+                    return false;
+                });
+                $nextButton.on('click', function() {
+                    $this[namespace]('slide', data.currentIndex + data.transitionStep);
+                    return false;
+                });
+
+                if ($pagination.length) {
+                    var $paginationUL = $('<ul></ul>');
+                    $paginationUL.on('click', 'li', function() {
+                        $this[namespace]('slide', $(this).index());
+                        return false;
+                    });
+                    data.$slidesLI.each(function() {
+                        $('<li></li>').appendTo($paginationUL);
+                    });
+                    data.$paginationLI = $paginationUL.children();
+                    data.$paginationLI.eq(data.currentIndex).addClass(GLOBALS.activeClass);
+                    $paginationUL.appendTo($pagination);
+                }
+
+                $this.data(namespace, data);
+
+                if(data.autoPlay) {
+                    $this[namespace]('play');
+                }
+            });
+        },
+
+        getCurrentIndex: function() {
+            var data = this.data(namespace);
+            return data.currentIndex;
+        },
+
+        slide: function(newIndex) {
+            return this.each(function() {
+                var $this = $(this);
+                var data = $this.data(namespace);
+
+                if(!data.transitionRunning && data.currentIndex != newIndex) {
+                    data.transitionRunning = true;
+                    data.transitionStartCallback.call($this, newIndex);
+
+                    newIndex = mod(newIndex, data.$slidesLI.length);
+
+                    var transitionEndFunction = function() {
+                        data.transitionRunning = false;
+                        data.transitionEndCallback.call($this, newIndex);
+                        $this[namespace]('reset');
+                    };
+
+                    if (data.transitionEffect == 'fade') {
+                        var firstFadeTransition = function() {
+                            return data.$slidesLI.eq(data.currentIndex).fadeTo(data.transitionDuration, 0, data.transitionEasing).css({'z-index': 0});
+                        },
+                        secondFadeTransition = function() {
+                            return data.$slidesLI.eq(newIndex).fadeTo(data.transitionDuration, 1, data.transitionEasing).css({'z-index': 1});
+                        };
+
+                        if (data.transitionCrossfade) {
+                            $.when(function(){firstFadeTransition(); secondFadeTransition(); return data.$slidesLI;}()).done(transitionEndFunction);
+                        } else {
+                            $.when(firstFadeTransition()).done(function(){$.when(secondFadeTransition()).done(transitionEndFunction);});
+                        }
+                    } else {
+                        data.$slidesUL.animate(data.transitionCssProperty == 'left' ? {'left': -newIndex * data.slidesLISize} : {'top': -newIndex * data.slidesLISize}, data.transitionDuration, data.transitionEasing, transitionEndFunction);
+                    }
+
+                    data.$slidesLI.removeClass(GLOBALS.activeClass).eq(newIndex).addClass(GLOBALS.activeClass);
+                    if (data.$paginationLI) {
+                        data.$paginationLI.removeClass(GLOBALS.activeClass).eq(newIndex).addClass(GLOBALS.activeClass);
+                    }
+
+                    data.currentIndex = newIndex;
+                }
+            });
+        },
+
+        play: function() {
+            return this.each(function() {
+                var $this = $(this);
+                var data = $this.data(namespace);
+
+                if (!data.isPlaying) {
+                    data.isPlaying = true;
+                    clearTimeout(data.timer);
+                    data.timeStart = $.now();
+                    data.timer = setTimeout(function() {
+                        $this[namespace]('slide', data.currentIndex + data.transitionStep);
+                    }, data.timeRemaining);
+                }
+            });
+        },
+
+        pause: function() {
+            return this.each(function() {
+                var $this = $(this);
+                var data = $this.data(namespace);
+
+                if (data.isPlaying) {
+                    data.isPlaying = false;
+                    clearTimeout(data.timer);
+                    if (data.timeStart) {
+                        data.timeRemaining = data.timeRemaining - ($.now() - data.timeStart);
+                    }
+                }
+            });
+        },
+
+        reset: function() {
+            return this.each(function() {
+                var $this = $(this);
+                var data = $this.data(namespace);
+
+                data.timeRemaining = data.displayDuration;
+                data.isPlaying = false;
+                $this[namespace]('play');
+            });
+        }
+    };
+
+    function mod(num, modulo) {
+        var remain = num % modulo;
+        return Math.floor(remain >= 0 ? remain : remain + modulo);
+    }
+
+    $.fn[namespace] = function(method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.' + namespace);
+        }
+    };
+
+})(jQuery);
